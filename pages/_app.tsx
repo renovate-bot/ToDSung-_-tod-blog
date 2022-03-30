68afc57546afb18824c296a89cbbefdf277567c3
ToD <wlunareve@gmail.com>
2022-03-31 00:32:58 +0800

    feat: add head title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,20 @@ import '@/styles/globals.css';
 
 import type { AppProps } from 'next/app';
 
+import Head from '@/components/Head';
 import Navbar from '@/components/Navbar';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <main id='app' className='app flex justify-center	text-justify'>
-      <Navbar />
-      <div className='component-wrapper relative mt-[3.25rem] w-[80vw] max-w-[1680px]'>
-        <Component {...pageProps} />
-      </div>
-    </main>
+    <>
+      <Head title="Tod's personal blog from TW" />
+      <main id='app' className='app flex justify-center	text-justify'>
+        <Navbar />
+        <div className='component-wrapper relative mt-[3.25rem] w-[80vw] max-w-[1680px]'>
+          <Component {...pageProps} />
+        </div>
+      </main>
+    </>
   );
 }
 
