fd266562bed7968ab90eddc7493681ad87f8d3f3
ToD <wlunareve@gmail.com>
2022-04-21 00:10:31 +0800

    feat: add some functional programming code day2

--- pages/FP/index.tsx
+++ pages/FP/index.tsx
@@ -1,72 +1,122 @@
-import { get } from 'lodash/fp';
 import filter from 'lodash/fp/filter';
 import flow from 'lodash/fp/flow';
+import get from 'lodash/fp/get';
 import isEqual from 'lodash/fp/isEqual';
+import some from 'lodash/fp/some';
 
 const fileName = 'image.png';
 
-const getFileExtension: (fileName: string) => string = fileName =>
-  fileName.split('.').slice(-1)[0];
+const isEndWithCsv: (fileName: string) => boolean = fileName =>
+  fileName.endsWith('.csv');
 
-const isFileCsv = flow([
-  getFileExtension, //
-  isEqual('csv'),
+const isEndWithPng: (fileName: string) => boolean = fileName =>
+  fileName.endsWith('.png');
+
+const isStringFile = isEqual('file');
+const isStringDirectory = isEqual('directory');
+
+type directoryObj = {
+  name: string;
+  children?: directoryObj[];
+  kind: string;
+  file?: File | null;
+  layer: number;
+};
+
+const directoryStructure: directoryObj = {
+  name: 'root',
+  children: [
+    {
+      name: 'label.csv',
+      kind: 'file',
+      layer: 1,
+    },
+    {
+      name: 'images',
+      kind: 'directory',
+      children: [
+        {
+          name: '001.png',
+          kind: 'file',
+          layer: 2,
+        },
+        {
+          name: '002.png',
+          kind: 'file',
+          layer: 2,
+        },
+        {
+          name: '003.png',
+          kind: 'file',
+          layer: 2,
+        },
+      ],
+      layer: 1,
+    },
+  ],
+  kind: 'directory',
+  file: null,
+  layer: 0,
+};
+const { children } = directoryStructure;
+
+const isFile = flow([
+  get('kind'), //
+  isStringFile,
 ]);
 
-const isFilePng = flow([
-  getFileExtension, //
-  isEqual('png'),
-]);
+const filterFile = filter(isFile);
 
-const isDirectory = isEqual('directory');
-
-const children = [
-  {
-    name: 'image.png',
-    kind: 'file',
-  },
-  {
-    name: 'label.csv',
-    kind: 'file',
-  },
-  {
-    name: 'images',
-    kind: 'directory',
-  },
-];
-
-const filterFileCsv = filter(
+const isFileCsv: (directory: directoryObj) => boolean = directory =>
   flow([
     get('name'), //
-    isFileCsv,
-  ])
-);
+    isEndWithCsv,
+  ])(directory) && isFile(directory);
+
+const filterFileCsv = filter(isFileCsv);
 
 const filterFilePng = filter(
   flow([
     get('name'), //
-    isFilePng,
+    isEndWithPng,
   ])
 );
 
 const filterDirectory = filter(
   flow([
     get('kind'), //
-    isDirectory,
+    isStringDirectory,
   ])
 );
+
+const isLayer: (layer: number) => Function = layer =>
+  flow([
+    get('layer'), //
+    isEqual(layer),
+  ]);
+
+const isLayer1 = isLayer(1);
+const isLayer2 = isLayer(2);
+
+const isLabelCsvExist = some(isLayer1 && isFileCsv);
 
 const HomeFP = () => {
   return (
     <main className='map__container h-[calc(100vh-3.5rem)] md:h-[calc(100vh-3.25rem)]'>
-      <div>isFileCsv: {`${isFileCsv(fileName)}`}</div>
-      <div>isFilePng: {`${isFilePng(fileName)}`}</div>
+      <div>isEndWithCsv: {`${isEndWithCsv(fileName)}`}</div>
+      <div>isEndWithPng: {`${isEndWithPng(fileName)}`}</div>
       <br />
       <div>original children: {JSON.stringify(children)} </div>
+      <div>
+        filterFile: {`${JSON.stringify(filterFile(children?.[1].children))}`}{' '}
+      </div>
       <div>filterFileCsv: {`${JSON.stringify(filterFileCsv(children))}`}</div>
       <div>filterFilePng: {`${JSON.stringify(filterFilePng(children))}`}</div>
       <div>
         filterDirectory: {`${JSON.stringify(filterDirectory(children))}`}
+      </div>
+      <div>
+        isLabelCsvExist: {`${JSON.stringify(isLabelCsvExist(children))}`}
       </div>
     </main>
   );
