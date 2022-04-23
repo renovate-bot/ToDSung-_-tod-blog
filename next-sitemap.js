a8eee982244c94ebf69b93b7ceef1524054e523b
ToD <wlunareve@gmail.com>
2022-04-23 23:20:35 +0800

    feat: modify sitemap xml tag order

--- next-sitemap.js
+++ next-sitemap.js
@@ -3,4 +3,14 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://tod-blog.vercel.app/',
   generateRobotsTxt: true,
+
+  transform: async (config, path) => {
+    return {
+      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      changefreq: config.changefreq,
+      priority: config.priority,
+      alternateRefs: config.alternateRefs ?? [],
+    };
+  },
 };
