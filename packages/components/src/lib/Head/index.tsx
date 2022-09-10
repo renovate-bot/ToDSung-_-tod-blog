ab40f91d31b7e51fd25b7e64f26ca938e6968ad4
ToD <wlunareve@gmail.com>
2022-09-10 23:44:13 +0800

    refactor: split head component to curi from tod-blog

---
+++ packages/components/src/lib/Head/index.tsx
@@ -0,0 +1,51 @@
+import Head from 'next/head';
+import { ReactNode } from 'react';
+
+type Props = {
+  title: string;
+  author: string;
+  description: string;
+  url: string;
+  favicon?: string;
+  children?: ReactNode;
+};
+
+const HtmlHead = ({
+  title,
+  author,
+  description,
+  url,
+  favicon,
+  children,
+}: Props) => {
+  return (
+    <Head>
+      <meta charSet='UTF-8' />
+      <title>{title}</title>
+      <meta name='description' content={description} />
+      <meta name='author' content={author} />
+      <meta property='og:title' content={description} />
+      <meta property='og:description' content={description} />
+      <meta property='og:type' content='website' />
+      <meta property='og:url' content={url} />
+      <meta property='og:locale' content='zh-tw' />
+      {favicon && <link rel='icon' href={favicon} />}
+      <link
+        rel='preload'
+        href='/fonts/JasonHandwriting/JasonHandwriting1.ttf'
+        as='font'
+        crossOrigin=''
+      />
+      <link
+        rel='preload'
+        href='/fonts/DiamorScript/Diamor.ttf'
+        as='font'
+        crossOrigin=''
+      />
+      <link rel='alternate' hrefLang='zh-Hant' href={url} />
+      {children}
+    </Head>
+  );
+};
+
+export default HtmlHead;
--- packages/tod-blog/components/Head/index.tsx
+++ packages/tod-blog/components/Head/index.tsx
@@ -1,52 +1,31 @@
-import { FC } from 'react';
-import Head from 'next/head';
+import Head from '@curi/components/Head';
 
 type Props = {
   title: string;
   description: string;
 };
 
-const HtmlHead: FC<Props> = ({
+const HtmlHead = ({
   title,
   description = 'I am ToD 努力嘗試分享的小小前端，希望這邊有任何一篇文章能夠幫助到你！',
-}) => {
+}: Props) => {
   const titleContent = title
-    ? `${title} - Tod'blog`
+    ? `${title} - Tod's blog`
     : "Tod's personal blog from TW";
 
   return (
-    <Head>
-      <meta charSet='UTF-8' />
-      <title>{titleContent}</title>
-      <meta name='description' content={description} />
-      <meta name='author' content='Tod Sung' />
-      <meta property='og:title' content={titleContent} />
-      <meta property='og:description' content={description} />
-      <meta property='og:type' content='website' />
-      <meta property='og:url' content='https://tod-blog.vercel.app/' />
-      <meta property='og:locale' content='zh-tw' />
+    <Head
+      title={titleContent}
+      description={description}
+      author='ToD Sung'
+      favicon='/favicon.ico'
+      url='https://tod-blog.vercel.app/'
+    >
       <meta
         name='google-site-verification'
         content='9JFlPPjMcTWCa_ePEuHyFvlCv8LS2xZkeK3alcNc_oE'
       />
       <link rel='icon' href='/favicon.ico' />
-      <link
-        rel='alternate'
-        hrefLang='zh-Hant'
-        href='https://tod-blog.vercel.app/'
-      />
-      <link
-        rel='preload'
-        href='/fonts/JasonHandwriting/JasonHandwriting1.ttf'
-        as='font'
-        crossOrigin=''
-      />
-      <link
-        rel='preload'
-        href='/fonts/DiamorScript/Diamor.ttf'
-        as='font'
-        crossOrigin=''
-      />
     </Head>
   );
 };
