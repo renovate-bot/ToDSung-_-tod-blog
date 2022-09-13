75a93fa840b997186c61cf1e12a13545cf09dc19
ToD <wlunareve@gmail.com>
2022-09-13 23:10:00 +0800

    build: fix next-sitemap path

--- packages/tod-blog/next-sitemap.js
+++ packages/tod-blog/next-sitemap.js
@@ -1,9 +1,12 @@
 /** @type {import('next-sitemap').IConfig} */
 
+const path = require('path');
+
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://tod-blog.vercel.app/',
   generateRobotsTxt: true,
-
+  sourceDir: path.join(process.cwd(), '../../dist/packages/tod-blog/.next'),
+  outDir: path.join(process.cwd(), '../../dist/packages/tod-blog/public'),
   transform: async (config, path) => {
     return {
       loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
